8ddb848e3ac7d4f5035b3080bc298d005a557c2d
arturoruge <arturoruge@gmail.com>
2017-01-06 13:28:59 -0300

    add normalize, html5shiv, gifs and animate.css

--- js/actions.js
+++ js/actions.js
@@ -1,8 +1,37 @@
-	function myFunction(){
-	var x = document.getElementById("myTopnav");
-	if (x.className === "topnav") {
-		x.className += " responsive";	
-	} else {
-		x.className = "topnav";
-	}
-}
+			$(function() {
+      $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+      $('html,body').animate({
+      scrollTop: target.offset().top
+      }, 1000);
+      return false;
+      }
+      }
+      });
+      //:::::::::::WAYPOINT FUNCTION
+      $('.aparecer').waypoint(function() {
+		$('.aparecer').addClass( 'fadeInUp' );
+		},
+		{
+			offset: '50%',
+			triggerOnce: true
+		});
+      $('.aparecer2').waypoint(function() {
+		$('.aparecer2').addClass( 'fadeInUp' );
+		},
+		{
+			offset: '50%',
+			triggerOnce: true
+		});
+      $('.aparecer3').waypoint(function() {
+		$('.aparecer3').addClass( 'fadeInDown' );
+		},
+		{
+			offset: '50%',
+			triggerOnce: true
+		});
+      });
+
